dc35909e5602f3bf046343a0682c8bfcd1c8e78c
Thomas Andrus <thomasmandrus@gmail.com>
2026-04-23 10:58:47 -0500

    CH 2 - L5

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document\n")
+    print(f"Found {num_words} total words\n")
     sorted_list = sorted([{'letter': k, 'num': v} for k, v in num_chars.items()], key=lambda x: x['num'], reverse=True)
     for element in sorted_list:
         print(f"The '{element['letter']}' character was found {element['num']} times")
@@ -12,10 +14,6 @@ def main():
 
 def sort_on(dict):
     return dict[""]
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 def get_num_chars(text):
     chars = {}
